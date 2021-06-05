894fc8d270f5d29309ba6a5ec828c68c72148614
Arun <kosana.arun@zestmoney.in>
2021-06-05 20:28:32 +0530

    correcting linting errors

--- app/core/models.py
+++ app/core/models.py
@@ -8,7 +8,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
         if not email:
-          raise ValueError('Users must have an email address')
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -16,13 +16,14 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-      """Creates and saves a new super user"""
-      user = self.create_user(email, password)
-      user.is_staff = True
-      user.is_superuser = True
-      user.save(using = self._db)
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-      return user
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
